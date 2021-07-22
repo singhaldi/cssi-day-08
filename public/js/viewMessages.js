22d46ecc2c3be82fee2909cf464929dc3b232127
Divya <singhaldivya11@gmail.com>
2021-07-22 16:46:01 +0000

    read data from databse

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,7 +1,10 @@
-console.log("got here");
-
 const getMessages = () => {
     const passcode = document.querySelector("#passcode");
-    console.log("button pressed", passcode.value);
+    
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+    })
 
 }
