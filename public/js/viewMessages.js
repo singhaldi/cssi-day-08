0f1e667688016fdefc58a38b9f4fd10926f421ad
Divya <singhaldivya11@gmail.com>
2021-07-23 07:43:51 +0000

    Hash password working

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,16 +1,20 @@
 const getMessages = () => {
     const passcode = document.querySelector("#passcode");
-    let numTries = 0;
-    let matched = false;
     
-    
+    // password string
+    var str = passcode.value;
+    // new MD5 instance and hexadecimal string encoding
+    var MD5 = new Hashes.MD5().hex(str);
+    console.log(MD5);
+
+
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
     
         for (let key in data){
-            if (data[key].passcode === passcode.value){
+            if (data[key].passcode === MD5){
                 console.log("match found");
                 const message = document.querySelector("#message");
                 message.innerHTML = data[key].message;
--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -1,52 +1,21 @@
 const onSubmit = () => {
 
+
+
     //console.log("submit button clicked");
     const passcodeInput = document.querySelector("#passcode").value;
     const messageInput = document.querySelector("#message").value;
     console.log(passcodeInput);
     console.log(messageInput);
 
-    function utf8Encode(str) {
-    var x, y, output = '',
-      i = -1,
-      l;
-
-    if (str && str.length) {
-      l = str.length;
-      while ((i += 1) < l) {
-        /* Decode utf-16 surrogate pairs */
-        x = str.charCodeAt(i);
-        y = i + 1 < l ? str.charCodeAt(i + 1) : 0;
-        if (0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF) {
-          x = 0x10000 + ((x & 0x03FF) << 10) + (y & 0x03FF);
-          i += 1;
-        }
-        /* Encode output as utf-8 */
-        if (x <= 0x7F) {
-          output += String.fromCharCode(x);
-        } else if (x <= 0x7FF) {
-          output += String.fromCharCode(0xC0 | ((x >>> 6) & 0x1F),
-            0x80 | (x & 0x3F));
-        } else if (x <= 0xFFFF) {
-          output += String.fromCharCode(0xE0 | ((x >>> 12) & 0x0F),
-            0x80 | ((x >>> 6) & 0x3F),
-            0x80 | (x & 0x3F));
-        } else if (x <= 0x1FFFFF) {
-          output += String.fromCharCode(0xF0 | ((x >>> 18) & 0x07),
-            0x80 | ((x >>> 12) & 0x3F),
-            0x80 | ((x >>> 6) & 0x3F),
-            0x80 | (x & 0x3F));
-        }
-      }
-    }
-    return output;
-  }
-
-  let newPasscode = utf8Encode(passcodeInput);
-  console.log(newPasscode);
+    // password string
+    var str = passcodeInput;
+    // new MD5 instance and hexadecimal string encoding
+    var MD5 = new Hashes.MD5().hex(str);
+    console.log(MD5);
 
     const payload = {
-        passcode: passcodeInput, 
+        passcode: MD5, 
         message: messageInput
     };
 
